b93a7278a92a446cd56df5022bb028c9415b9a0f
khai96_ <hvksmr1996@gmail.com>
2016-03-13 20:52:05 +0700

    a little update

--- test/main.js
+++ test/main.js
@@ -10,9 +10,9 @@
 	console.log(test_result.version);
 
 	if (test_result.error) {
-		process.stderr.write(`TEST FAILED.\n${test_result.error}\n`);
+		process.stderr.write(`TEST FAILED\n${test_result.error}\n`);
 	} else {
-		process.stdout.write(`TEST SUCCEED.\n`);
+		process.stdout.write(`TEST SUCCEED\n`);
 		process.stdout.write(`\x1B[32mprocess.stdout:\x1B[0m\n${test_result.stdout}\n`);
 		process.stdout.write(`\x1B[32mprocess.stderr:\x1B[0m\n${test_result.stderr}\n`);
 	}
