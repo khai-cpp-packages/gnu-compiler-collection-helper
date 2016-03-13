4e625dfe4054d80f9cc27021208b9b599d583542
khai96_ <hvksmr1996@gmail.com>
2016-03-13 20:38:15 +0700

    create tester

---
+++ test/index.js
@@ -0,0 +1,29 @@
+
+((module) => {
+	'use strict';
+
+	var child_process = require('child_process');
+	var gnu = require('..');
+
+	var test_result = Object.create(null);
+
+	test_result.version = gnu.cplusplus.version();
+
+	try {
+		let throwIf = (error) => {
+			if (error) throw error;
+		};
+		let compile_result = gnu.cplusplus(['-Wall', '-Wextra', gnu.cplusplus.INCLUDE('./include')], './main.exe', './main.cpp');
+		throwIf(compile_result.error);
+		throwIf(compile_result.stderr);
+		test_result.out = String(compile_result.stdout);
+		let execute_result = child_process.spawnSync('./main.exe');
+		test_result.stdout = execute_result.stdout;
+		test_result.stderr = execute_result.stderr;
+	} catch (error) {
+		test_result.error = error;
+	}
+
+	module.exports = test_result;
+
+})(module);
---
+++ test/main.js
@@ -0,0 +1,4 @@
+
+process.chdir(__dirname);
+console.log('Testing: gnu-compiler-collection-helper');
+console.log(require('.'));
