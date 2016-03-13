9821fe3ff761af8ab3fb404c1db8084b631dc833
khai96_ <hvksmr1996@gmail.com>
2016-03-13 17:55:22 +0700

    update

--- g++.js
+++ g++.js
@@ -9,10 +9,10 @@
 	const DEFAULT_FLAGS = freeze(['-Wall', '-Wextra']);
 	const DEFAULT_OUTPUT = 'compiled-binary.out';
 
-	var compile = (flags, output, input) =>
+	var compile = (flags, output, input, options) =>
 		spawnSync(COMPILER, [...flags, `-o`, `"${output}"`, ...input]);
 
-	module.exports = (flags, output, input) =>
-		compile(flags || DEFAULT_FLAGS, output || DEFAULT_OUTPUT, typeof input === 'object' ? input : [input || 'main.cpp']);
+	module.exports = (flags, output, input, options) =>
+		compile(flags || DEFAULT_FLAGS, output || DEFAULT_OUTPUT, typeof input === 'object' ? input : [input || 'main.cpp'], options);
 
 })(module);
