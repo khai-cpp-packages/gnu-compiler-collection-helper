61ec5addd247643c14104bb13e20d56616dcd4f9
khai96_ <hvksmr1996@gmail.com>
2016-03-14 13:38:51 +0700

    update tester

--- test/main.js
+++ test/main.js
@@ -10,14 +10,14 @@
 
 		var sync = spawn('node', ['--es-staging', `./${type.toLowerCase()}/main.js`]);
 
-		sync.on('error', (error) => process.stderr.write(`TEST ${tname.toUpperCase()} FAILED\n${error}\n`));
-		sync.on('exit', (code) => process.stdout.write(`Test Sync exited with code ${code}\n`));
+		sync.on('error', (error) => process.stderr.write(`TEST ${type.toUpperCase()} FAILED\n${error}\n`));
+		sync.on('exit', (code) => process.stdout.write(`Test ${type} exited with code ${code}\n`));
 		sync.stdout.on('data', (chunk) => process.stdout.write(String(chunk)));
 		sync.stderr.on('data', (chunk) => process.stderr.write(String(chunk)));
 
 	});
 
-	test('async');
-	test('sync');
+	test('aSync');
+	test('Sync');
 
 })();
