b86c4c8f2e46720f4f354482f8682e17ce6dfabd
khai96_ <hvksmr1996@gmail.com>
2016-03-14 09:18:34 +0700

    update

--- test/async/index.js
+++ test/async/index.js
@@ -2,6 +2,30 @@
 ((module) => {
 	'use strict';
 
-	var child_process = require('child_process');
+	var spawn = require('child_process').spawn;
+	var compile = require('../../g++.js');
+
+	compile.version((version) => process.stderr.write(`\n\x1B[92m${version}\x1B[0m`));
+
+	var flags = [
+		'-Wall', '-Wextra',
+		compile.INCLUDE('./include'),
+		compile.STANDARD('c++14')
+	];
+
+	module.exports = new Promise((resolve, reject) => {
+
+		var child = compile(flags, './main.exe', './main.cpp');
+
+		child.on('exit', () => {
+			var child = spawn('./main.exe');
+			child.on('error', reject);
+			child.on('exit', () => resolve(child));
+		});
+
+		child.on('error', reject);
+		child.stderr.on('error', reject);
+
+	});
 
 })(module);
